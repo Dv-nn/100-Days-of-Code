a11d84feebd4d77bd548e05e8687c3f51a2049bb
Natalya <79689641+Dv-nn@users.noreply.github.com>
2023-11-23 20:00:41 +0300

    Create main.py

---
+++ Day 1/main.py
@@ -0,0 +1,7 @@
+
+def generator_band_name():
+  print("Welcome to the Band Name Generator")
+  city_name = print("What's name of the city you grew up in?")
+  pet_name = print("What's your pet's name?")
+  final = city_name + "_" + pet_name
+  print(f"Your band name could be {final}")
