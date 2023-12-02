ee6744fb9606d0c9c99d5fb3828c3ffc5588bf52
Natalya <79689641+Dv-nn@users.noreply.github.com>
2023-12-02 17:45:17 +0300

    Update main.py

--- Day 17/quiz-game/main.py
+++ Day 17/quiz-game/main.py
@@ -1,1 +1,11 @@
+from question_modal import Question
+from data  import question_data
+
+question_bank = []
+for question in question_data:
+  question_text = question["text"]
+  question_answer = question["answer"]
+  new_question = Question(question_text, question_answer)
+  question_bank.append(new_question)
+
 
