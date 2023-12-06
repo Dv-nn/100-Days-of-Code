432f33c38b0a5261d81e8f38a04ae3fe41b930b5
Natalya <79689641+Dv-nn@users.noreply.github.com>
2023-12-06 14:26:53 +0300

    Update snake_game.py

--- Day 21/snake_game.py
+++ Day 21/snake_game.py
@@ -1,6 +1,7 @@
-from turtle import Screen
-import time
+from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
