3d7e14966504af53ad97adef06e96a575caf65b7
Natalya <79689641+Dv-nn@users.noreply.github.com>
2023-12-08 14:05:48 +0300

    Update main.py

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,2 +1,29 @@
 file = open("my_file.txt")
 contents = file.rad()
+print(contest)
+file.close()
+
+# ____________________________
+with open("my_file.txt") as file:
+  contents = file.read()
+  print(contest)
+
+# ____________________________
+with open("my_file.txt", mode="w") as file:
+  file.write("\nNew text")
+
+
+#______________________________
+PLACEHOLDER =["name"]
+
+with open("...txt") as names_file:
+  names = names_file.readlines()
+
+with open(...txt) as letter_file:
+  letter_contents = letter_file.read()
+  for name in names:
+    stripped_name = name.strip()
+    new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+    with open(f"..{stripped_name}.txt", mode="w") as completed_letter:
+      completed_letter.write(new_letter)
+    
